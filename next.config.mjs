414e064ad089176a6e3b92ec7ae96e99718fb8c7
Max Wolfs <mail@maxwolfs.com>
2024-10-11 11:36:21 +0200

    add static export config
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+
+    // For static export and i18n configuration
     i18n: {
         locales: ['de', 'en'],
         defaultLocale: 'en',
     },
+
+    // These settings are important for GitHub Pages
+    basePath: '/sovereigncloudstack.org', // Replace with your repository name
+    assetPrefix: '/sovereigncloudstack.org/', // Replace with your repository name
+    trailingSlash: true, // Adds trailing slash to every route to work with GitHub Pages
+
+    // Optional: Disable server-side rendering completely to ensure static export
+    output: 'export', // Enable static export
 };
 
 export default nextConfig;
